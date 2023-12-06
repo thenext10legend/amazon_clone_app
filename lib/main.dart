811d839c2a2f0c2c2041456780c6813852ee0165
Shivam JhunJhunwala <shivamjhunjhunwala4dec@gmail.com>
2023-12-06 14:32:21 +0530

    global variables colours added

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:amazon_clone_app/constants/global_variables.dart';
+import 'package:amazon_clone_app/features/auth/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+      ),
+      initialRoute: '/authScreen',
+      routes: {
+        '/authScreen': (context) => const AuthScreen(),
+      },
+    );
+  }
+}
