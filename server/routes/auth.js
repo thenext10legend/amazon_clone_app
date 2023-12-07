c1102566a2fef33aacbdd0b246f2c9fda2d3d619
Shivam JhunJhunwala <shivamjhunjhunwala4dec@gmail.com>
2023-12-07 23:22:23 +0530

    creating user Schema and Model

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,27 @@
 const express=require('express');
+const User= require("../models/user")
 
 const authRouter = express.Router();
 
-authRouter.post('/api/signup', (req,res)=>{
+authRouter.post('/api/signup', async (req,res)=>{
     //get the data from the client
     const {name,email,password}= req.body;
 
-    
+    //checking if the email already exists
+    const existingUser=await User.findOne({email});
+    if(existingUser){
+        res.status(400).json({msg: "User with same email already exists!"})
+    }
+
+    //creating user model
+    let user = new User({
+        email,
+        password,
+        name,
+    })
+
+    user=await user.save();
+    res.json(user);
     //post that data in database
 
     //return that data to the user
